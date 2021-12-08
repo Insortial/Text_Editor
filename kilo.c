938fac21740bea770d50dd5219feac3786b5830c
INSORTIAL <renwell2099@gmail.com>
2021-12-08 07:48:29 +0000

    Turned off echoing

--- kilo.c
+++ kilo.c
@@ -1,3 +1,23 @@
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+struct termios orig_termios;
+
+void disableRawMode(){}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+    enableRawMode();
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
